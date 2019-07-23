f57c4c52096fc0127c46660c5ce918ef8a8767dc
BINDAL Vivek <vivek.bindal@soprasteria.com>
2019-07-23 20:12:05 +0530

    make models

--- src/model.py
+++ src/model.py
@@ -1,23 +1,26 @@
 import numpy as np
-from keras.models import Sequential
+from keras.models import Model
 from keras import layers
 from keras.optimizers import RMSprop
 
 
 from recurrent_advanced import AttLSTMCond
 import prepData as PD
+import constants as CONST
 
 
 TRAIN_SPLIT = 500000
 
-PATH = "D:\\ML\\translate\\data\\"
-PATH = PD.PATH
+PATH = CONST.PATH
 MODEL_PATH = PATH + "models\\"
 
 
 def getData():
-	x,y = PD.loadVectorizedData()
-		
+	x = []
+	y = []
+	
+	#x,y = PD.loadVectorizedData()
+	
 	xTrain = x[:TRAIN_SPLIT]
 	xTest = x[TRAIN_SPLIT:]
 	yTrain = y[:TRAIN_SPLIT]
@@ -26,73 +29,86 @@ def getData():
 	return xTrain,yTrain,xTest,yTest
 
 
-def trainLSTMAttModel(xTrain,yTrain):
-
+def translationLSTMAttModel():
 	INPUT_SENTENCE_LENGTH = 64
 	OUTPUT_SENTENCE_LENGTH = 1
 	INPUT_WORDS_COUNT = 10000
 	OUTPUT_WORDS_COUNT = 10000
 	EMBEDDING_SIZE = 128
-	ENCODER_LSTM_UNITS = 128
-	DECODER_LSTM_UNITS = 128
-	ATTENTION_SIZE = 128
-	NEXT_WORD_SIZE = 128
+	NUM_LSTM_UNITS = 128
 
+	########################
+	#training model creation start
 	encoderInput = layers.Input(batch_shape=(None,INPUT_SENTENCE_LENGTH))
 	encoderEmbedding = layers.Embedding(input_dim=INPUT_WORDS_COUNT, output_dim=EMBEDDING_SIZE, input_length=INPUT_SENTENCE_LENGTH)(encoderInput)
-	encoderOut = layers.Bidirectional(layers.LSTM(ENCODER_LSTM_UNITS, return_sequence=True))(encoderEmbedding)
+	encoderOut = layers.Bidirectional(layers.LSTM(NUM_LSTM_UNITS, return_sequence=True))(encoderEmbedding)
 	
 	decoderInput = layers.Input(batch_shape=(None,OUTPUT_SENTENCE_LENGTH))
 	decoderEmbedding = layers.Embedding(input_dim=OUTPUT_WORDS_COUNT, output_dim=EMBEDDING_SIZE, input_length=OUTPUT_SENTENCE_LENGTH)(decoderInput)
-	
+		
 	initialDecodeState = layers.Lambda(lambda x: x[:,-1,:])(encoderOut)
 	initialDecodeState = layers.Flatten(axis=1)(initialDecodeState)
 	
-	attentionLayer = AttLSTMCond(ENCODER_LSTM_UNITS, return_extra_variables=True, return_states=True, return_sequences=True)
-	[proj_h, x_att, alphas, h_state] = attentionLayer([decoderEmbedding,encoderOut,initialDecodeState])
+	attentionLayer_SHARED = AttLSTMCond(NUM_LSTM_UNITS, return_extra_variables=True, return_states=True, return_sequences=True)
+	[proj_h, x_att, alphas, h_state] = attentionLayer_SHARED([decoderEmbedding,encoderOut,initialDecodeState])
 	
 	############attention implementation
 	#decoderLSTM, decoderStates = layers.LSTM(DECODER_LSTM_UNITS, return_states=True)(decoderEmbedding)
 	#decoderToAtt = layers.RepeatVector(INPUT_SENTENCE_LENGTH)(decoderLSTM)
 	#attentionInput = layers.Concat([encoderOut,decoderToAtt])
-	#attentionFC = layers.Dense(ATTENTION_SIZE)(attentionInput)
+	#attentionFC = layers.Dense(NUM_LSTM_UNITS)(attentionInput)
 	#attentionFC = layers.Dense(1)(attentionFC)
 	#attentionAlpha = layers.Activation("softmax")(attentionFC)
 	#attentionAlpha = layers.Flaten()(attentionAlpha)
-	#attentionAlpha = layers.RepeatVector(ENCODER_LSTM_UNITS)(attentionAlpha)
+	#attentionAlpha = layers.RepeatVector(NUM_LSTM_UNITS)(attentionAlpha)
 	#contextOut = layers.Multiply([encoderOut,attentionAlpha])
 
-	
-	decoderState = layers.TimeDistributed(layers.Dense(EMBEDDING_SIZE))(proj_h)
-	contextVector = layers.TimeDistributed(layers.Dense(EMBEDDING_SIZE))(x_att)
-	prevPrediction = layers.TimeDistributed(layers.Dense(EMBEDDING_SIZE))(decoderEmbedding)
-	
-	
+	decoderState_SHARED = layers.TimeDistributed(layers.Dense(EMBEDDING_SIZE))
+	decoderState = decoderState_SHARED(proj_h)
+	contextVector_SHARED = layers.TimeDistributed(layers.Dense(EMBEDDING_SIZE))
+	contextVector = contextVector_SHARED(x_att)
+	prevPrediction_SHARED = layers.TimeDistributed(layers.Dense(EMBEDDING_SIZE))
+	prevPrediction = prevPrediction_SHARED(decoderEmbedding)
 	
 	wordOut = layers.Add(activation='relu')([contextVector,decoderState,prevPrediction])
-	wordOut = layers.TimeDistributed(layers.Dense(EMBEDDING_SIZE))(wordOut)
-	wordOut = layers.TimeDistributed(layers.Dense(OUTPUT_WORDS_COUNT, activation="softmax"))(wordOut)
+	wordOut_SHARED_1 = layers.TimeDistributed(layers.Dense(EMBEDDING_SIZE))
+	wordOut = wordOut_SHARED_1(wordOut)
+	wordOut_SHARED_2 = layers.TimeDistributed(layers.Dense(OUTPUT_WORDS_COUNT, activation="softmax"))
+	wordOut = wordOut_SHARED_2(wordOut)
 	
 	trainingModel = Model(inputs=[encoderInput,decoderInput],outputs=[wordOut])
 	
+
+
+	################################
+	#sampling model creation start
+	samplingModelInit = Model(inputs=[encoderInput,decoderInput],outputs=[wordOut,encoderOut,h_state,alphas])
+
+	preprocessedEncoder = layers.Input(shape=(None,INPUT_SENTENCE_LENGTH*2))			#since Bi-LSTM
+	previousAttState = layers.Input(shape=(None,INPUT_SENTENCE_LENGTH))
+
+
+	[proj_h, x_att, alphas, h_state] = attentionLayer_SHARED([decoderEmbedding,preprocessedEncoder,previousAttState])
+
+	decoderState = decoderState_SHARED(proj_h)
+	contextVector = contextVector_SHARED(x_att)
+	prevPrediction = prevPrediction_SHARED(decoderEmbedding)
 	
+	wordOut = layers.Add(activation='relu')([contextVector,decoderState,prevPrediction])
+	wordOut = wordOut_SHARED_1(wordOut)
+	wordOut = wordOut_SHARED_2(wordOut)
 	
-	
-	#model.summary()
-	trainingModel.compile(	optimizer=RMSprop(lr=8e-4),
-					loss='categorical_crossentropy',
-					metrics=['acc'])
-	
-	
-	history = trainingModel.fit(xTrain, yTrain,
-					epochs=40,
-					batch_size=128,
-					validation_split=0.2)
-	
-	trainingModel.summary()
-	saveModel(trainingModel, "training_model")
-	
-	return trainingModel
+	samplingModelNext = Model(inputs=[preprocessedEncoder,previousAttState,decoderInput],outputs=[wordOut,preprocessedEncoder,h_state,alphas])
+
+	samplingModel = [samplingModelInit, samplingModelNext]
+
+
+
+	trainingModel.compile(optimizer=RMSprop(lr=8e-4),loss='categorical_crossentropy',metrics=['acc'])
+	samplingModel[0].compile(optimizer=RMSprop(lr=8e-4),loss='categorical_crossentropy',metrics=['acc'])
+	samplingModel[1].compile(optimizer=RMSprop(lr=8e-4),loss='categorical_crossentropy',metrics=['acc'])
+
+	return trainingModel, samplingModel
 	
 
 def saveModel(model,modelName):
@@ -106,12 +122,24 @@ def saveModel(model,modelName):
 	print("Saved model to disk")
 	
 
-	
 if __name__ == "__main__":
 	xTrain,yTrain,xTest,yTest = getData()
-	model = trainConvModel(xTrain,yTrain)
+	trainingModel, samplingModel = translationLSTMAttModel()
 
-	#scores = model.evaluate(xTest, yTest, verbose=0)
-	#print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+	trainingModel.summary()
+	samplingModel[0].summary()
+	samplingModel[1].summary()
+
+
+	# history = trainingModel.fit(xTrain, yTrain,
+	# 				epochs=50,
+	# 				batch_size=128,
+	# 				validation_split=0.2)
+
+	# saveModel(trainingModel,"training_model")
+	# saveModel(samplingModel,"sampling_model")
+
+	# scores = trainingModel.evaluate(xTest, yTest, verbose=0)
+	# print("%s: %.2f%%" % (trainingModel.metrics_names[1], scores[1]*100))
 
 
